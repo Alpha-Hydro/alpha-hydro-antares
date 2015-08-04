b9d19187306f186e5a29037bdd1f0c6a620b19da
vladmeh <vladmeh@gmail.com>
2015-08-04 11:56:24 +0300

    test webhook

--- application/modules/default/controllers/WebhookController.php
+++ application/modules/default/controllers/WebhookController.php
@@ -10,7 +10,7 @@ class WebhookController extends Zend_Controller_Action
 {
     public function indexAction(){
 
-        if($this->getRequest()->isPost() && $_POST['payload']){
+        if($this->getRequest()->isPost() && $this->getRequest()->getPost('payload')){
             $local_repo = APPLICATION_ROOT;
             shell_exec("cd {$local_repo} && git pull");
         }
