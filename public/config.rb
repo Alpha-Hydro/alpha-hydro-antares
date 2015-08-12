9a15b973b8a4f3b9590874fa2c66986018eaff0e
vladmeh <vladmeh@gmail.com>
2015-08-12 17:16:30 +0300

    public buttons

--- public/config.rb
+++ public/config.rb
@@ -11,15 +11,14 @@ images_dir = "img"
 javascripts_dir = "js"
 
 # http_stylesheets_dir = "public/css/"
-# http_images_dir = "../img/"
-# http_images_dir = File.join(http_path, "..", "img")
+# http_images_dir = "public/img/"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
