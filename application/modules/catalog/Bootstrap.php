9c1e840c8403cd366e38d7747526852ef38a1454
vladmeh <vladmeh@gmail.com>
2015-09-10 13:42:54 +0300

    route ini

--- application/modules/catalog/Bootstrap.php
+++ application/modules/catalog/Bootstrap.php
@@ -13,7 +13,7 @@ class Catalog_Bootstrap extends Zend_Application_Module_Bootstrap
         $front = Zend_Controller_Front::getInstance();
         $router = $front->getRouter();
 
-        $config = new Zend_Config_Ini(APPLICATION_PATH . "/modules/" . $this->getModuleName() . '/configs/route.ini', 'production');
+        $config = new Zend_Config_Ini(APPLICATION_PATH . "/modules/" . strtolower($this->getModuleName()) . '/configs/route.ini', 'production');
         $router->addConfig($config,'routes');
     }
 }
