352420cf6b52b67b21e6b38bbf9af09b57cf3200
vladmeh <vladmeh@gmail.com>
2015-09-18 17:46:47 +0300

    Static page about and contacts

---
+++ application/modules/default/controllers/AboutController.php
@@ -0,0 +1,18 @@
+<?php
+
+class AboutController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        $this->view->title = 'О нас';
+    }
+
+    public function indexAction()
+    {
+
+    }
+
+
+}
+
---
+++ application/modules/default/controllers/ContactsController.php
@@ -0,0 +1,18 @@
+<?php
+
+class ContactsController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        $this->view->title = 'Контакты';
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+
+}
+
