eb11eb4c27e90a47b46367b5645e557bc8613ca1
vladmeh <vladmeh@gmail.com>
2015-10-06 10:00:48 +0300

    data base update media, media_categories

--- data/pages_media.sql
+++ data/pages_media.sql
@@ -1,2 +1,29 @@
 ALTER TABLE pages RENAME TO media;
 ALTER TABLE pages_categories RENAME TO media_categories;
+
+# Media
+ALTER TABLE media ADD path VARCHAR(255) NOT NULL DEFAULT '';
+ALTER TABLE media ADD content_markdown TEXT;
+ALTER TABLE media ADD meta_title VARCHAR(255);
+ALTER TABLE media ADD meta_description TEXT;
+ALTER TABLE media ADD meta_keywords TEXT;
+ALTER TABLE media ADD sorting INT DEFAULT 0 NOT NULL;
+ALTER TABLE media ADD active INT DEFAULT 1 NOT NULL;
+ALTER TABLE media ADD deleted INT DEFAULT 0 NOT NULL;
+
+ALTER TABLE media ADD CONSTRAINT unique_id UNIQUE (id);
+# После заполнения строк
+# ALTER TABLE media ADD CONSTRAINT unique_path UNIQUE (path);
+
+# Media_categories
+ALTER TABLE media_categories ADD path VARCHAR(255) NOT NULL DEFAULT '';
+ALTER TABLE media_categories ADD meta_title VARCHAR(255);
+ALTER TABLE media_categories ADD meta_description TEXT;
+ALTER TABLE media_categories ADD meta_keywords TEXT;
+ALTER TABLE media_categories ADD sorting INT DEFAULT 0 NOT NULL;
+ALTER TABLE media_categories ADD active INT DEFAULT 1 NOT NULL;
+ALTER TABLE media_categories ADD deleted INT DEFAULT 0 NOT NULL;
+
+ALTER TABLE media_categories ADD CONSTRAINT unique_id UNIQUE (id);
+# После заполнения строк
+# ALTER TABLE media_categories ADD CONSTRAINT unique_path UNIQUE (path);
---
+++ scripts/migrations/005-Media.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: mvl
+ * Date: 21.09.2015
+ * Time: 10:14
+ */
+class MediaCategories extends Akrabat_Db_Schema_AbstractChange
+{
+    function up()
+    {
+        $tableName = $this->_tablePrefix . 'media_categories';
+
+        $sql = "
+                ALTER TABLE $tableName ADD path VARCHAR(255) NOT NULL DEFAULT '';
+                ALTER TABLE $tableName ADD meta_title VARCHAR(255);
+                ALTER TABLE $tableName ADD meta_description TEXT;
+                ALTER TABLE $tableName ADD meta_keywords TEXT;
+                ALTER TABLE $tableName ADD sorting INT DEFAULT 0 NOT NULL;
+                ALTER TABLE $tableName ADD active INT DEFAULT 1 NOT NULL;
+                ALTER TABLE $tableName ADD deleted INT DEFAULT 0 NOT NULL;
+                ALTER TABLE $tableName ADD CONSTRAINT unique_id UNIQUE (id);
+            ";
+
+        $this->_db->query($sql);
+    }
+
+    function down()
+    {
+        $tableName = $this->_tablePrefix . 'media_categories';
+
+        $sql = "
+                ALTER TABLE $tableName DROP path;
+                ALTER TABLE $tableName DROP meta_title;
+                ALTER TABLE $tableName DROP meta_description;
+                ALTER TABLE $tableName DROP meta_keywords;
+                ALTER TABLE $tableName DROP sorting;
+                ALTER TABLE $tableName DROP active;
+                ALTER TABLE $tableName DROP deleted;
+            ";
+
+        $this->_db->query($sql);
+    }
+}
