10816fb77995f973a46b9f720bfe5c2fde599da9
vladmeh <vladmeh@gmail.com>
2015-10-07 17:36:27 +0300

    Create db table oil

---
+++ data/oil.sql
@@ -0,0 +1,20 @@
+CREATE TABLE oil
+(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    category_id INT DEFAULT 0 NOT NULL,
+    title VARCHAR(255) DEFAULT '' NOT NULL,
+    path VARCHAR(128) DEFAULT '' NOT NULL,
+    descroption TEXT,
+    content_html TEXT,
+    content_markdown TEXT,
+    image VARCHAR(128),
+    date_create TIMESTAMP,
+    meta_title VARCHAR(255),
+    meta_description TEXT,
+    meta_keywords VARCHAR(255),
+    active TINYINT DEFAULT 1 NOT NULL,
+    sorting INT DEFAULT 0 NOT NULL,
+    deleted TINYINT DEFAULT 0 NOT NULL
+);
+ALTER TABLE oil ADD CONSTRAINT unique_id UNIQUE (id);
+ALTER TABLE oil ADD CONSTRAINT unique_path UNIQUE (path);
