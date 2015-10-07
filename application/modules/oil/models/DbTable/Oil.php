c5beb613db5598e01a3b67dbd7c3894dcd0dab02
vladmeh <vladmeh@gmail.com>
2015-10-07 17:38:54 +0300

    create models mapper Oil

---
+++ application/modules/oil/models/DbTable/Oil.php
@@ -0,0 +1,10 @@
+<?php
+
+class Oil_Model_DbTable_Oil extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = 'oil';
+
+
+}
+
---
+++ application/modules/oil/models/mappers/Oil.php
@@ -0,0 +1,153 @@
+<?php
+
+class Oil_Model_Mapper_Oil
+{
+
+    protected $_dbTable = null;
+
+    /**
+     * @param $dbTable
+     * @return $this
+     * @throws Exception
+     */
+    public function setDbTable($dbTable)
+    {
+        if (is_string($dbTable))
+        	$dbTable = new $dbTable();
+        
+        if (!$dbTable instanceof Zend_Db_Table_Abstract)
+        	throw new Exception('Invalid table data gateway provided');
+        
+        $this->_dbTable = $dbTable;
+        
+        return $this;
+    }
+
+    /**
+     * @return Oil_Model_DbTable_Oil
+     */
+    public function getDbTable()
+    {
+        if (null === $this->_dbTable)
+        	$this->setDbTable('Oil_Model_DbTable_Oil');
+        
+        return $this->_dbTable;
+    }
+
+    /**
+     * @param Oil_Model_Oil $oil
+     * @return $this
+     */
+    public function save(Oil_Model_Oil $oil)
+    {
+        $data = $this->_getDbData($oil);
+        
+        if (null == ($id = $oil->getId())) {
+        	unset($data[$this->_getDbPrimary()]);
+        	$this->getDbTable()->insert($data);
+        } else {
+        	$this->getDbTable()->update($data, array($this->_getDbPrimary(). ' = ?' => $id));
+        }
+        
+        return $this;
+    }
+
+    /**
+     * @param $id
+     * @param Oil_Model_Oil $oil
+     * @return Oil_Model_Oil|null
+     */
+    public function find($id, Oil_Model_Oil $oil)
+    {
+        $result = $this->getDbTable()->find($id);
+        
+        if (0 == count($result)) {
+        	return null;
+        }
+        
+        $row = $result->current();
+        $entry = $this->_setDbData($row, $oil);
+        
+        return $entry;
+    }
+
+    /**
+     * @param null $select
+     * @return array
+     */
+    public function fetchAll($select = null)
+    {
+        $resultSet = $this->getDbTable()->fetchAll($select);
+        
+        $entries   = array();
+        foreach ($resultSet as $row) {
+        	$entry = new Oil_Model_Oil();
+        	$entry = $this->_setDbData($row, $entry);
+        	$entries[] = $entry;
+        }
+        
+        return $entries;
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function _getDbPrimary()
+    {
+        $primaryKey = $this->getDbTable()->info('primary');
+        
+        return $primaryKey[1];
+    }
+
+    /**
+     * @param Oil_Model_Oil $oil
+     * @return array
+     */
+    protected function _getDbData(Oil_Model_Oil $oil)
+    {
+        $info = $this->getDbTable()->info();
+        $properties = $info['cols'];
+        
+        $data = array();
+        foreach ($properties as $property) {
+        	$name = $this->_normaliseName($property);
+        
+        	if($property != $this->_getDbPrimary())
+        		$data[$property] = $oil->__get($name);
+        }
+        
+        return $data;
+    }
+
+    /**
+     * @param Zend_Db_Table_Rowset $row
+     * @param Oil_Model_Oil $entry
+     * @return Oil_Model_Oil
+     */
+    protected function _setDbData($row, Oil_Model_Oil $entry)
+    {
+        $info = $this->getDbTable()->info();
+        $properties = $info['cols'];
+        
+        foreach ($properties as $property) {
+        	$entry->__set($this->_normaliseName($property), $row->$property);
+        }
+        
+        return $entry;
+    }
+
+    /**
+     * @param $property string
+     * @return string
+     */
+    protected function _normaliseName($property)
+    {
+        $name = preg_split('~_~', $property);
+        $normaliseName = implode(array_map('ucwords', $name));
+        
+        return $normaliseName;
+    }
+
+
+}
+
