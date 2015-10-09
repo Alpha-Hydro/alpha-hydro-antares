dcf695892cfb345a87483b0a4d3ae0063078d871
vladmeh <vladmeh@gmail.com>
2015-10-09 12:06:07 +0300

    Create modute manufacture. DB table manufacture, manufacture-categories. Mapper models manufacture, manufactureCategories

---
+++ application/modules/manufacture/models/DbTable/Manufacture.php
@@ -0,0 +1,10 @@
+<?php
+
+class Manufacture_Model_DbTable_Manufacture extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = 'manufacture';
+
+
+}
+
---
+++ application/modules/manufacture/models/DbTable/ManufactureCategories.php
@@ -0,0 +1,10 @@
+<?php
+
+class Manufacture_Model_DbTable_ManufactureCategories extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = 'manufacture_categories';
+
+
+}
+
