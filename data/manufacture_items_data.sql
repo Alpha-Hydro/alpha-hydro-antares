5dc81ddfc75b53bb8d8a8fd69811d7a2fb65d58a
vladmeh <vladmeh@gmail.com>
2015-10-13 17:51:57 +0300

    Manufacture items data db

---
+++ data/manufacture_items_data.sql
@@ -0,0 +1,185 @@
+INSERT INTO manufacture (id, category_id, title, path, description, content_html, content_markdown, image, create_date, meta_title, meta_description, meta_keywords, active, sorting, deleted, full_path) VALUES (3, 1, 'Стандартное исполнение', 'standartnoe_ispolnenie', 'Может применяться в различных приводах промышленной и мобильной гидравлики', '<ul>
+<li>ГЦ 80-30-930</li>
+<li>Давление 200 кг/см2.</li>
+<li>Усилие на штоке при рабочем давлении не менее:</li>
+<li>
+<ul>
+<li>в поршневой полости 10000 кгс.</li>
+</ul></li>
+<li>
+<ul>
+<li>в штоковой полости 8500 кгс.</li>
+</ul></li>
+<li>Диаметр поршня 80 мм.</li>
+<li>Диаметр штока 30 мм.</li>
+<li>Рабочий ход штока 930мм.</li>
+</ul>
+', '- ГЦ 80-30-930
+- Давление 200 кг/см2.
+- Усилие на штоке при рабочем давлении не менее:
+- - в поршневой полости 10000 кгс.
+- - в штоковой полости 8500 кгс.
+-  Диаметр поршня 80 мм.
+- Диаметр штока 30 мм.
+- Рабочий ход штока 930мм.', '/upload/manufacture/items/production-9.jpg', '2015-10-13 15:58:58', 'Стандартное исполнение', 'Стандартное исполнение. Может применяться в различных приводах промышленной и мобильной гидравлики', '', 1, 0, 0, 'gidrocilindry/standartnoe_ispolnenie');
+INSERT INTO manufacture (id, category_id, title, path, description, content_html, content_markdown, image, create_date, meta_title, meta_description, meta_keywords, active, sorting, deleted, full_path) VALUES (4, 1, 'Специальное исполнение', 'specialnoe_ispolnenie', 'Гидроцилиндр для зажима буровой штанги', '<ul>
+<li>ГЦ 125-60-50</li>
+<li>Давление 160 бар.</li>
+<li>Усилие на штоке при рабочем давлении не менее:
+
+<ul>
+<li>в поршневой полости 19000 кгс.</li>
+<li>в штоковой полости 15000 кгс.</li>
+</ul></li>
+<li>Диаметр поршня 125мм.</li>
+<li>Диаметр штока 60 мм.</li>
+<li>Рабочий ход штока 50мм.</li>
+</ul>
+', '* ГЦ 125-60-50
+* Давление 160 бар.
+* Усилие на штоке при рабочем давлении не менее:
+  - в поршневой полости 19000 кгс.
+  - в штоковой полости 15000 кгс.
+* Диаметр поршня 125мм.
+* Диаметр штока 60 мм.
+* Рабочий ход штока 50мм.', '/upload/manufacture/items/production-10.jpg', '2015-10-13 15:59:57', 'Специальное исполнение', 'Специальное исполнение. Гидроцилиндр для зажима буровой штанги', '', 1, 0, 0, 'gidrocilindry/specialnoe_ispolnenie');
+INSERT INTO manufacture (id, category_id, title, path, description, content_html, content_markdown, image, create_date, meta_title, meta_description, meta_keywords, active, sorting, deleted, full_path) VALUES (5, 2, 'Гидростанция для испытания', 'gidrostancija_dlja_ispytanija', 'Гидростанция для испытания трубопроводов большого диаметра', '<ul>
+<li>Объем бака 100л</li>
+<li>Расход 5 л/мин./75 л/мин.</li>
+<li>Давление 220 Бар</li>
+<li>Мощность электродвигателя 4кВт / 2,2кВт (1500/380)</li>
+<li>Сфера: Нефтегазовая промышленность</li>
+</ul>
+', '* Объем бака 100л
+* Расход 5 л/мин./75 л/мин.
+* Давление 220 Бар
+* Мощность электродвигателя 4кВт / 2,2кВт (1500/380)
+* Сфера: Нефтегазовая промышленность', '/upload/manufacture/items/production-1.jpg', '2015-10-13 16:03:55', 'Гидростанция для испытания', 'Гидростанция для испытания трубопроводов большого диаметра', '', 1, 0, 0, 'gidrostancii/gidrostancija_dlja_ispytanija');
+INSERT INTO manufacture (id, category_id, title, path, description, content_html, content_markdown, image, create_date, meta_title, meta_description, meta_keywords, active, sorting, deleted, full_path) VALUES (6, 2, 'Гидравлический привод станка ЧПУ', 'gidravlicheskij_privod_stanka_chpu', 'Гидравлический привод станка ЧПУ', '<ul>
+<li>Наименование: ГС 30-5,5-200-2,2</li>
+<li>Объём бака 30 литров.</li>
+<li>Расход 5,5 л/мин.</li>
+<li>Давление 200 Бар</li>
+<li>Мощность электродвигателя 2,2 кВт (1500/380)</li>
+<li>Сфера: Станкостроение</li>
+</ul>
+', '* Наименование: ГС 30-5,5-200-2,2
+* Объём бака 30 литров.
+* Расход 5,5 л/мин.
+* Давление 200 Бар
+* Мощность электродвигателя 2,2 кВт (1500/380)
+* Сфера: Станкостроение', '/upload/manufacture/items/production-2.jpg', '2015-10-13 16:40:40', 'Гидравлический привод станка ЧПУ', 'Гидравлический привод станка ЧПУ', '', 1, 0, 0, 'gidrostancii/gidravlicheskij_privod_stanka_chpu');
+INSERT INTO manufacture (id, category_id, title, path, description, content_html, content_markdown, image, create_date, meta_title, meta_description, meta_keywords, active, sorting, deleted, full_path) VALUES (7, 2, 'Напорная станция', 'napornaja_stancija', 'Напорная станция для подачи компонентов битума на стройплощадку', '<ul>
+<li>Расход: 55 л/мин. / 75 л/мин.</li>
+<li>Давление: 20 Бар</li>
+<li>Мощность электродвигателя: 2,2 кВт / 0,75 кВт (1500/220)</li>
+<li>Сфера: Строительство</li>
+</ul>
+', '* Расход: 55 л/мин. / 75 л/мин.
+* Давление: 20 Бар
+* Мощность электродвигателя: 2,2 кВт / 0,75 кВт (1500/220)
+* Сфера: Строительство', '/upload/manufacture/items/production-3.jpg', '2015-10-13 16:43:40', 'Напорная станция', 'Напорная станция для подачи компонентов битума на стройплощадку', '', 1, 0, 0, 'gidrostancii/napornaja_stancija');
+INSERT INTO manufacture (id, category_id, title, path, description, content_html, content_markdown, image, create_date, meta_title, meta_description, meta_keywords, active, sorting, deleted, full_path) VALUES (8, 2, 'Гидропривод станка модели ТС912Ф3', 'gidroprivod_stanka_modeli_ts912f3', 'Гидропривод станка модели ТС912Ф3. ТС912Ф3 является специальным колесотокарным станком', '<ul>
+<li>Объём бака: 100 л</li>
+<li>Расход: 15 л/мин / 25 л/мин</li>
+<li>Давление: 100 Бар</li>
+<li>Мощность электродвигателя 1,5 кВт / 2,2 кВт (1500/380)</li>
+</ul>
+', '* Объём бака: 100 л
+* Расход: 15 л/мин / 25 л/мин
+* Давление: 100 Бар
+* Мощность электродвигателя 1,5 кВт / 2,2 кВт (1500/380)', '/upload/manufacture/items/production-4.jpg', '2015-10-13 16:46:50', 'Гидропривод станка модели ТС912Ф3', 'Гидропривод станка модели ТС912Ф3. ТС912Ф3 является специальным колесотокарным станком', '', 1, 0, 0, 'gidrostancii/gidroprivod_stanka_modeli_ts912f3');
+INSERT INTO manufacture (id, category_id, title, path, description, content_html, content_markdown, image, create_date, meta_title, meta_description, meta_keywords, active, sorting, deleted, full_path) VALUES (9, 2, 'Напорная станция с резервным насосом', 'napornaja_stancija_s_rezervnym_nasosom', 'Напорная станция для подачи компонентов битума на стройплощадку с резервным насосом', '<ul>
+<li>Расход: 55л/мин / 55л/мин / 75л/мин</li>
+<li>Давление: 20 Бар</li>
+<li>Мощность: 2,2кВт / 2,2 кВт / 0,75кВт (1500/220)</li>
+</ul>
+', '* Расход: 55л/мин / 55л/мин / 75л/мин
+* Давление: 20 Бар
+* Мощность: 2,2кВт / 2,2 кВт / 0,75кВт (1500/220)', '/upload/manufacture/items/production-5.jpg', '2015-10-13 17:03:47', 'Напорная станция с резервным насосом', 'Напорная станция для подачи компонентов битума на стройплощадку с резервным насосом', '', 1, 0, 0, 'gidrostancii/napornaja_stancija_s_rezervnym_nasosom');
+INSERT INTO manufacture (id, category_id, title, path, description, content_html, content_markdown, image, create_date, meta_title, meta_description, meta_keywords, active, sorting, deleted, full_path) VALUES (10, 2, 'Универсальная станция', 'universalnaja_stancija', 'Универсальная станция с двумя агрегатами', '<ul>
+<li>Объем бака 100л</li>
+<li>Расход 15 л/мин; 11 л/мин</li>
+<li>Давление 180 Бар</li>
+<li>Мощность 5,5 кВт / 5,5 кВт</li>
+<li>Сфера: Универсальная</li>
+</ul>
+', '* Объем бака 100л
+* Расход 15 л/мин; 11 л/мин
+* Давление 180 Бар
+* Мощность 5,5 кВт / 5,5 кВт
+* Сфера: Универсальная', '/upload/manufacture/items/production-6.jpg', '2015-10-13 17:24:04', 'Универсальная станция с двумя агрегатами', 'Универсальная станция с двумя агрегатами', '', 1, 0, 0, 'gidrostancii/universalnaja_stancija');
+INSERT INTO manufacture (id, category_id, title, path, description, content_html, content_markdown, image, create_date, meta_title, meta_description, meta_keywords, active, sorting, deleted, full_path) VALUES (11, 2, 'Гидростанция для переработки', 'gidrostancija_dlja_pererabotki', 'Гидростанция для переработки древесины в пеллеты', '<ul>
+<li>Объем бака: 600 л</li>
+<li>Расход 197 л/мин</li>
+<li>Мощность электродвигателя 75 кВт</li>
+<li>Давление: 190 Бар</li>
+<li>Сфера: Деревопереработка</li>
+</ul>
+', '* Объем бака: 600 л
+* Расход 197 л/мин
+* Мощность электродвигателя 75 кВт
+* Давление: 190 Бар
+* Сфера: Деревопереработка', '/upload/manufacture/items/production-7.jpg', '2015-10-13 17:28:24', 'Гидростанция для переработки древесины', 'Гидростанция для переработки древесины в пеллеты', '', 1, 0, 0, 'gidrostancii/gidrostancija_dlja_pererabotki');
+INSERT INTO manufacture (id, category_id, title, path, description, content_html, content_markdown, image, create_date, meta_title, meta_description, meta_keywords, active, sorting, deleted, full_path) VALUES (12, 2, 'Гидростанция привода дробилки', 'gidrostancija_privoda_drobilki', 'Гидростанция привода дробилки в горнодобывающей промышленности', '<ul>
+<li>ГС 200-26-200-11</li>
+<li>Объём бака 200 литров.</li>
+<li>Расход 26 л/мин.</li>
+<li>Давление 200 Бар</li>
+<li>Мощность электродвигателя 11 кВт (1500/380)</li>
+</ul>
+', '* ГС 200-26-200-11
+* Объём бака 200 литров.
+* Расход 26 л/мин.
+* Давление 200 Бар
+* Мощность электродвигателя 11 кВт (1500/380)', '/upload/manufacture/items/production-8.jpg', '2015-10-13 17:08:07', 'Гидростанция привода дробилки', 'Гидростанция привода дробилки в горнодобывающей промышленности', '', 1, 0, 0, 'gidrostancii/gidrostancija_privoda_drobilki');
+INSERT INTO manufacture (id, category_id, title, path, description, content_html, content_markdown, image, create_date, meta_title, meta_description, meta_keywords, active, sorting, deleted, full_path) VALUES (13, 2, 'Гидростанция питания сервопривода ', 'gidrostancija_pitanija_servoprivoda', 'Гидростанция питания сервопривода направляющего аппарата гидротурбины ГЭС', '<ul>
+<li>Тип турбины Пл 60/5а-В-410</li>
+<li>Мощность турбины 65000 кВт</li>
+<li>Объём бака 4500 литров</li>
+<li>Давление 63 Бар</li>
+<li>Мощность электродвигателя 30,0 кВт (1500/380)</li>
+</ul>
+', '* Тип турбины Пл 60/5а-В-410
+* Мощность турбины 65000 кВт
+* Объём бака 4500 литров
+* Давление 63 Бар
+* Мощность электродвигателя 30,0 кВт (1500/380)', '/upload/manufacture/items/production-9.jpg', '2015-10-13 17:11:58', 'Гидростанция питания сервопривода ', 'Гидростанция питания сервопривода направляющего аппарата гидротурбины ГЭС', '', 1, 0, 0, 'gidrostancii/gidrostancija_pitanija_servoprivoda');
+INSERT INTO manufacture (id, category_id, title, path, description, content_html, content_markdown, image, create_date, meta_title, meta_description, meta_keywords, active, sorting, deleted, full_path) VALUES (14, 2, 'Гидростанция привода установки ЖБИ', 'gidrostancija_privoda_ustanovki_zhbi', 'Гидростанция привода кассетной установки ЖБИ', '<ul>
+<li>ГС 100-51-60-7,5</li>
+<li>Объём бака 100 литров.</li>
+<li>Расход 51 л/мин.</li>
+<li>Давление 60 Бар</li>
+<li>Мощность электродвигателя 7,5 кВт (1500/380)</li>
+</ul>
+', '* ГС 100-51-60-7,5
+* Объём бака 100 литров.
+* Расход 51 л/мин.
+* Давление 60 Бар
+* Мощность электродвигателя 7,5 кВт (1500/380)', '/upload/manufacture/items/production-10.jpg', '2015-10-13 17:39:25', 'Гидростанция привода установки ЖБИ', 'Гидростанция привода кассетной установки ЖБИ', '', 1, 0, 0, 'gidrostancii/gidrostancija_privoda_ustanovki_zhbi');
+INSERT INTO manufacture (id, category_id, title, path, description, content_html, content_markdown, image, create_date, meta_title, meta_description, meta_keywords, active, sorting, deleted, full_path) VALUES (15, 2, 'Гидростанция управления задвижками', 'gidrostancija_upravlenija_zadvizhkami', 'Гидростанция для управления задвижками большого диаметра', '<ul>
+<li>ГС 9-12-150-1</li>
+<li>Объем бака 9 л</li>
+<li>Расход 12 л/мин</li>
+<li>Давление 150 Бар</li>
+<li>Ручной привод</li>
+<li>Сфера: водоснабжение</li>
+</ul>
+', '* ГС 9-12-150-1
+* Объем бака 9 л
+* Расход 12 л/мин
+* Давление 150 Бар
+* Ручной привод
+* Сфера: водоснабжение', '/upload/manufacture/items/production-11.jpg', '2015-10-13 17:40:38', 'Гидростанция управления задвижками', 'Гидростанция для управления задвижками большого диаметра', '', 1, 0, 0, 'gidrostancii/gidrostancija_upravlenija_zadvizhkami');
+INSERT INTO manufacture (id, category_id, title, path, description, content_html, content_markdown, image, create_date, meta_title, meta_description, meta_keywords, active, sorting, deleted, full_path) VALUES (16, 2, 'Гидростанция привода кассетной установки', 'gidrostancija_privoda_kassetnoj_ustanovki', 'Гидростанция привода кассетной установки ЖБИ', '<ul>
+<li>ГС 75-20-60-3</li>
+<li>Объём бака 75 литров.</li>
+<li>Расход 20 л/мин.</li>
+<li>Давление 60 Бар</li>
+<li>Мощность электродвигателя 3,0 кВт (1500/380)</li>
+</ul>
+', '* ГС 75-20-60-3
+* Объём бака 75 литров.
+* Расход 20 л/мин.
+* Давление 60 Бар
+* Мощность электродвигателя 3,0 кВт (1500/380)', '/upload/manufacture/items/production-12.jpg', '2015-10-13 17:42:14', 'Гидростанция привода кассетной установки', 'Гидростанция привода кассетной установки ЖБИ', '', 1, 0, 0, 'gidrostancii/gidrostancija_privoda_kassetnoj_ustanovki');
