c46dab037658e7f013c6195edc46364323ca6c89
vladmeh <vladmeh@gmail.com>
2015-11-16 02:25:27 +0300

    test typescript

--- public/js/test.js
+++ public/js/test.js
@@ -8,7 +8,7 @@ var Table = (function () {
         return document.querySelector(this.className);
     };
     Table.prototype.tableTr = function () {
-        return [].slice.call(this.table().querySelectorAll('tr'));
+        return [].slice.call(this.table().querySelector('tbody').querySelectorAll('tr'));
     };
     Table.prototype.selectTr = function (classAlert) {
         if (classAlert === void 0) { classAlert = '.success'; }
@@ -16,17 +16,42 @@ var Table = (function () {
     };
     return Table;
 })();
-//document.body.innerHTML = greeter(person);
+var Button = (function () {
+    function Button(className) {
+        this.className = className;
+    }
+    Button.prototype.buttonGroup = function () {
+        return document.querySelector(this.className);
+    };
+    Button.prototype.buttons = function (tagName) {
+        if (tagName === void 0) { tagName = 'button'; }
+        return [].slice.call(this.buttonGroup().querySelectorAll(tagName));
+    };
+    return Button;
+})();
+function btnGroupDisabled(isDisable) {
+    if (isDisable === void 0) { isDisable = false; }
+    var btnGroup = new Button('.btn-group');
+    btnGroup.buttons().forEach(function (el) {
+        if (!classie.has(el, 'no-disabled'))
+            el.disabled = isDisable;
+        console.log(el.disabled);
+    });
+}
+function selectedTr() {
+    return btnGroupDisabled((table.selectTr()) ? false : true);
+}
 var table = new Table('.table');
 table.tableTr().forEach(function (tr) {
     tr.addEventListener('click', function (ev) {
         ev.preventDefault();
         //if (table.selectTr()) table.selectTr().classList.remove('success');
-        if (table.selectTr())
+        if (table.selectTr() && !classie.has(this, 'success'))
             classie.remove(table.selectTr(), 'success');
         //this.classList.add('success');
         classie.toggle(this, 'success');
+        selectedTr();
     });
 });
-//console.log(tableTr);
+//btnGroupDisabled(false);
 //# sourceMappingURL=test.js.map
--- public/typescript/test.ts
+++ public/typescript/test.ts
@@ -7,26 +7,49 @@ class Table {
         return document.querySelector(this.className);
     }
     tableTr(){
-        return [].slice.call(this.table().querySelectorAll('tr'));
+        return [].slice.call(this.table().querySelector('tbody').querySelectorAll('tr'));
     }
     selectTr(classAlert: string = '.success'){
         return this.table().querySelector(classAlert);
     }
 }
 
-//document.body.innerHTML = greeter(person);
+class Button {
+    constructor(public className: string){}
+    buttonGroup(){
+        return document.querySelector(this.className);
+    }
+    buttons(tagName: string = 'button'){
+        return [].slice.call(this.buttonGroup().querySelectorAll(tagName))
+    }
+}
+
+function btnGroupDisabled(isDisable: boolean = false){
+    var btnGroup = new Button('.btn-group');
+    btnGroup.buttons().forEach(function(el){
+        if(!classie.has(el,'no-disabled'))
+            el.disabled = isDisable;
+        console.log(el.disabled);
+    });
+}
+
+function selectedTr(){
+    return btnGroupDisabled((table.selectTr())?false:true);
+}
+
 var table = new Table('.table');
 
 table.tableTr().forEach(function(tr){
     tr.addEventListener('click', function(ev){
         ev.preventDefault();
         //if (table.selectTr()) table.selectTr().classList.remove('success');
-        if (table.selectTr()) classie.remove(table.selectTr(),'success');
+        if (table.selectTr() && !classie.has(this, 'success'))
+            classie.remove(table.selectTr(),'success');
         //this.classList.add('success');
         classie.toggle(this, 'success');
+        selectedTr();
     })
 });
-
-//console.log(tableTr);
+//btnGroupDisabled(false);
 
 
