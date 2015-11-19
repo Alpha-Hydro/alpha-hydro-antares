dae47bcc3022606dd57be966476ba44ee0b9025a
vladmeh <vladmeh@gmail.com>
2015-11-20 02:13:07 +0300

    pipeline property edit test

--- public/js/pipelinePropertyEdit.js
+++ public/js/pipelinePropertyEdit.js
@@ -5,54 +5,61 @@ var PipelineEdit = (function () {
         this.tableId = tableId;
         this.table = document.getElementById(this.tableId);
         this.row = [].slice.call(this.table.querySelector('tbody').querySelectorAll('tr'));
-        this.buttons = {
-            edit: [].slice.call(this.table.querySelectorAll('button.btn-property-edit')),
-            deleted: [].slice.call(this.table.querySelectorAll('button.btn-property-delete')),
-            save: [].slice.call(this.table.querySelectorAll('button.btn-property-save')),
-            add: this.table.querySelector('button.btn-property-add')
-        };
+        this.current = 0;
         this._init();
     }
     PipelineEdit.prototype._init = function () {
-        var table = this.table, btn = [];
+        var self = this, btn = [];
         this.row.forEach(function (tr) {
-            btn = [].slice.call(table.querySelectorAll('button'));
+            btn = [].slice.call(tr.querySelectorAll('button'));
+            btn.forEach(function (btn) {
+                btn.addEventListener('click', function () {
+                    self._initEvent(tr, this);
+                    self.current = self.row.indexOf(tr);
+                });
+            });
         });
-        btn.forEach(function (btn) {
-            btn.addEventListener('click', this._initEvent, false);
-        });
-        //this._initEvent(btn);
     };
-    PipelineEdit.prototype._initEvent = function (event) {
-        console.log(event);
+    PipelineEdit.prototype._initEvent = function (tr, btn) {
+        var btnEvent = btn.dataset.event;
+        if (btnEvent == 'edit') {
+            var tr_current = this.row[this.current];
+            if (!classie.has(btn, 'active')) {
+                this._reset(tr_current);
+                this._edit(tr, btn);
+            }
+            else {
+                this._reset(tr);
+            }
+        }
+    };
+    PipelineEdit.prototype._edit = function (tr, btn) {
+        var aInput = [].slice.call(tr.querySelectorAll('input')), btnSave = tr.querySelector('button[data-event="save"]');
+        classie.add(btn, 'active');
+        classie.remove(btnSave, 'hidden');
+        aInput.forEach(function (input) {
+            classie.remove(input, 'input-hidden');
+            classie.add(input, 'form-control');
+            classie.add(input, 'input-sm');
+            input.readOnly = false;
+        });
+        aInput[0].focus();
+    };
+    PipelineEdit.prototype._reset = function (tr) {
+        var aInput = [].slice.call(tr.querySelectorAll('input')), btn = tr.querySelector('button.active'), btnSave = tr.querySelector('button[data-event="save"]');
+        if (btn) {
+            classie.add(btnSave, 'hidden');
+            classie.remove(btn, 'active');
+            aInput.forEach(function (input) {
+                classie.add(input, 'input-hidden');
+                classie.remove(input, 'form-control');
+                classie.remove(input, 'input-sm');
+                input.readOnly = true;
+            });
+            btn.blur();
+        }
     };
     return PipelineEdit;
 })();
 var tableProperty = new PipelineEdit('pipelineProperty');
-//tableProperty.init();
-//console.log(tableProperty.buttons.edit);
-/*
-tableProperty.buttons.edit.forEach(function(el){
-    el.addEventListener('click', function(ev){
-        var btn:Element = ev.target,
-            rowTable = btn.parentElement.parentElement.parentElement,
-            rowInput = [].slice.call(rowTable.querySelectorAll('input'));
-
-        if(!classie.has(this, 'active')){
-            classie.add(this, 'active');
-
-            rowInput.forEach(function(el){
-                classie.remove(el, 'input-hidden');
-                classie.add(el, 'form-control');
-                $(el).prop('readonly', false);
-            });
-
-            rowInput[0].focus()
-        }
-
-
-
-        console.log(rowInput);
-    });
-});*/
 //# sourceMappingURL=pipelinePropertyEdit.js.map
--- public/typescript/pipelinePropertyEdit.ts
+++ public/typescript/pipelinePropertyEdit.ts
@@ -5,68 +5,77 @@ class PipelineEdit {
     table:any;
     row:any;
     buttons: any;
+    current: number;
     constructor(public tableId: string){
         this.table = document.getElementById(this.tableId);
         this.row = [].slice.call(this.table.querySelector('tbody').querySelectorAll('tr'));
-        this.buttons = {
-            edit: [].slice.call(this.table.querySelectorAll('button.btn-property-edit')),
-            deleted: [].slice.call(this.table.querySelectorAll('button.btn-property-delete')),
-            save: [].slice.call(this.table.querySelectorAll('button.btn-property-save')),
-            add: this.table.querySelector('button.btn-property-add'),
-        };
+        this.current = 0;
 
         this._init();
     }
 
     _init(){
-        var table = this.table,
+        var self = this,
             btn:any = [];
         this.row.forEach(function(tr){
-            btn = [].slice.call(table.querySelectorAll('button'));
+            btn = [].slice.call(tr.querySelectorAll('button'));
+            btn.forEach(function(btn){
+                btn.addEventListener('click',function(){
+                    self._initEvent(tr, this);
+                    self.current = self.row.indexOf(tr);
+                });
+            });
         });
-
-        btn.forEach(function(btn){
-            btn.addEventListener('click',this._initEvent, false);
-        });
-
-        //this._initEvent(btn);
     }
 
-    _initEvent(event:Event){
-        console.log(event);
+    _initEvent(tr, btn){
+        var btnEvent = btn.dataset.event;
+
+        if(btnEvent == 'edit'){
+            var tr_current = this.row[this.current];
+
+            if(!classie.has(btn, 'active')){
+                this._reset(tr_current);
+                this._edit(tr, btn);
+            }
+            else {
+                this._reset(tr);
+            }
+        }
     }
 
-    /*edit(event: Event): any{
-        console.log(event);
-    }*/
+    _edit(tr, btn){
+        var aInput:any = [].slice.call(tr.querySelectorAll('input')),
+            btnSave = tr.querySelector('button[data-event="save"]');
+        classie.add(btn,'active');
+        classie.remove(btnSave,'hidden');
+        aInput.forEach(function(input){
+            classie.remove(input, 'input-hidden');
+            classie.add(input, 'form-control');
+            classie.add(input, 'input-sm');
+            input.readOnly = false;
+        });
+        aInput[0].focus();
+    }
+
+    _reset(tr){
+        var aInput:any = [].slice.call(tr.querySelectorAll('input')),
+            btn = tr.querySelector('button.active'),
+            btnSave = tr.querySelector('button[data-event="save"]');
+
+        if(btn){
+            classie.add(btnSave,'hidden');
+            classie.remove(btn, 'active');
+            aInput.forEach(function(input){
+                classie.add(input, 'input-hidden');
+                classie.remove(input, 'form-control');
+                classie.remove(input, 'input-sm');
+                input.readOnly = true;
+            });
+            btn.blur();
+        }
+    }
 }
 
 var tableProperty = new PipelineEdit('pipelineProperty');
-//tableProperty.init();
 
-//console.log(tableProperty.buttons.edit);
-
-/*
-tableProperty.buttons.edit.forEach(function(el){
-    el.addEventListener('click', function(ev){
-        var btn:Element = ev.target,
-            rowTable = btn.parentElement.parentElement.parentElement,
-            rowInput = [].slice.call(rowTable.querySelectorAll('input'));
-
-        if(!classie.has(this, 'active')){
-            classie.add(this, 'active');
-
-            rowInput.forEach(function(el){
-                classie.remove(el, 'input-hidden');
-                classie.add(el, 'form-control');
-                $(el).prop('readonly', false);
-            });
-
-            rowInput[0].focus()
-        }
-
-
-
-        console.log(rowInput);
-    });
-});*/
