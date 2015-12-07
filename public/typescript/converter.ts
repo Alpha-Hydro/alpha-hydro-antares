2a19786d18e1df1a7dfcc0252c9cfeb8b68a2cc2
vladmeh <vladmeh@gmail.com>
2015-12-07 17:49:12 +0300

    converter box

--- public/typescript/converter.ts
+++ public/typescript/converter.ts
@@ -3,40 +3,115 @@
 
 class Converter {
     form: Element;
+    selectType: any;
+    div_value:Element;
+    input_value:any;
+    select_value:any;
+    div_result:Element;
+    input_result:any;
+    select_result:any;
 
     constructor(formId:string, public units: Object){
         this.form = document.getElementById(formId);
+        this.selectType = this.form.querySelector('select#select-type');
+        this.div_value = this.form.querySelector('div#value');
+        this.input_value = this.div_value.querySelector('input');
+        this.select_value = this.div_value.querySelector('select');
+        this.div_result = this.form.querySelector('div#result');
+        this.input_result = this.div_result.querySelector('input');
+        this.select_result = this.div_result.querySelector('select');
 
         this._init();
     }
     _init(){
         var self = this,
-            units = this.units,
-            select = document.createElement('select'),
-            div_content = document.createElement('div');
-        select.classList.add('form-control');
-        select.classList.add('mb1');
-        select.classList.add('input-sm');
-        div_content.classList.add('conv_content');
-        div_content.setAttribute('id', 'conv_content');
+            units = this.units;
+
+        this.input_result.readOnly = true;
 
         for(var i in units){
             if(units.hasOwnProperty(i)){
                 var text = units[i].title;
-                this.addOption(select, text, i);
+                this.addOption(this.selectType, text, i);
             }
         }
-        this.form.appendChild(select);
-        this.form.appendChild(div_content);
 
-        var currentType = select.options.item(select.selectedIndex).value;
+        var currentType = this.selectType.options.item(this.selectType.selectedIndex).value;
         this._changeUnitType(currentType);
 
-        select.addEventListener('change', function(ev){
+        this.selectType.addEventListener('change', function(ev){
             ev.preventDefault();
             var val:string = this.value;
             self._changeUnitType(val);
+            //currentType = val;
         });
+
+        //this._initEvent(currentType);
+    }
+
+    _changeUnitType(type:string){
+        var self = this,
+            typeUnits:any[] = this.units[type].u;
+
+        this.input_value.value = this.units[type].default;
+
+        this.select_value.innerHTML = '';
+        for(var i in typeUnits){
+            if(typeUnits.hasOwnProperty(i)){
+                var text = typeUnits[i][0];
+                this.addOption(this.select_value, text, typeUnits[i][1]);
+            }
+        }
+
+        this.select_result.innerHTML = '';
+        for(var i in typeUnits){
+            if(typeUnits.hasOwnProperty(i)){
+                var text = typeUnits[i][0];
+                this.addOption(self.select_result, text, typeUnits[i][1]);
+            }
+        }
+        this.select_result.options.item(1).selected = true;
+        this._convert();
+
+        this._initEvent(type);
+    }
+
+    _initEvent(type){
+        var self = this;
+
+        this.input_value.addEventListener('input', function(ev){
+            ev.preventDefault();
+            self._convert();
+        });
+
+        this.select_value.addEventListener('change', function(ev){
+            ev.preventDefault();
+            self._convert();
+            console.log(type);
+        });
+
+        this.select_result.addEventListener('change', function(ev){
+            ev.preventDefault();
+            self._convert();
+        });
+    }
+
+    _convert(){
+        var a = this.select_value.value,
+            b = this.select_result.value,
+            c = this.input_value.value,
+            //res = Math.round(((c*a)/b)*100)/100;
+            res = (c*a)/b;
+        this.input_result.value = this.numberFormat(res);
+    }
+
+    _convertTemp(){
+        var a = this.select_value.value,
+            b = this.select_result.value,
+            c = this.input_value.value,
+            //res = Math.round(((c*a)/b)*100)/100;
+            res = (c*a)/b;
+        this.input_result.value = this.numberFormat(res);
     }
 
     addOption = (oSelect, text, value, isDefaultSelected:boolean = false, isSelected:boolean = false) => {
@@ -50,54 +125,29 @@ class Converter {
         oSelect.appendChild(oOption);
     };
 
-    _changeUnitType(type:string){
-        var self = this,
-            div_content:any = self.form.querySelector('div#conv_content'),
-            typeUnits:any[] = this.units[type].u,
-            defaultUnitsValue:number = this.units[type].default;
-        div_content.innerHTML = '';
-        typeUnits.forEach(function(u){
-            var formGroup = document.createElement('div'),
-                inputGroup = document.createElement('div'),
-                inputUnit = document.createElement('input'),
-                unitAddon = document.createElement('span'),
-                unitValue:any = u[1]*defaultUnitsValue;
-
-            formGroup.classList.add('form-group');
-            formGroup.classList.add('mb1');
-
-            inputGroup.classList.add('input-group');
-            inputGroup.classList.add('input-group-sm');
-
-            inputUnit.type = 'text';
-            inputUnit.value = unitValue;
-            inputUnit.classList.add('form-control');
-
-            unitAddon.classList.add('input-group-addon');
-            unitAddon.appendChild(document.createTextNode(u[0]));
-
-            inputGroup.appendChild(inputUnit);
-            inputGroup.appendChild(unitAddon);
-            formGroup.appendChild(inputGroup);
-            div_content.appendChild(formGroup);
-        });
+    numberFormat = (number:any,decimal:number = 6) => {
+        var res = parseFloat(number),
+            exp10 = Math.pow(10,decimal);
+        res = Math.round(res*exp10)/exp10;
+        return res;
     }
+
 }
 
 var units = {
     'pressure':{
         'title':'Давление',
-        'default': 100,
+        'default': 1,
         'u': [
             ['бар',1],
-            //['мбар',0.001],
-            //['мкбар',0.000001],
             ['Па',0.00001],
-            //['кПа',0.01],
-            //['МПа',10],
+            ['мбар',0.001],
+            ['мкбар',0.000001],
+            ['кПа',0.01],
+            ['МПа',10],
             ['мм рт.ст.',0.00133322],
             ['мм вод.ст',0.0000980665],
-            //['м вод.ст',0.0980665],
+            ['м вод.ст',0.0980665],
             ['кг/мм2',98.0665],
             ['кг/см2',0.980665],
             ['атм',1.01325],
@@ -106,7 +156,7 @@ var units = {
     },
     'length':{
         'title':'Длина',
-        'default': 100,
+        'default': 1,
         'u': [
             ['м',1],
             ['см',0.01],
