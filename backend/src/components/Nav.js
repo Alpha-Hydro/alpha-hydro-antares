dd029ffb43a6d00bd8b8046c1165f7ecefca7304
vladmeh <vladmeh@gmail.com>
2016-02-17 18:00:54 +0300

    react admin

--- backend/src/components/Nav.js
+++ backend/src/components/Nav.js
@@ -1,28 +1,11 @@
 import React from "react";
 import ButtonList from "./ButtonList";
-import ReactBootstrap from "react-bootstrap";
 
 export default class Nav extends React.Component {
     render() {
         return(
             <nav className="affix side-nav right mw70">
               <ButtonList data={this.props.data}/>
-
-							<ReactBootstrap.Modal.Dialog>
-								<ReactBootstrap.Modal.Header>
-									<ReactBootstrap.Modal.Title>Modal title</ReactBootstrap.Modal.Title>
-								</ReactBootstrap.Modal.Header>
-
-								<ReactBootstrap.Modal.Body>
-									One fine body...
-								</ReactBootstrap.Modal.Body>
-
-								<ReactBootstrap.Modal.Footer>
-									<ReactBootstrap.Button>Close</ReactBootstrap.Button>
-									<ReactBootstrap.Button bsStyle="primary">Save changes</ReactBootstrap.Button>
-								</ReactBootstrap.Modal.Footer>
-
-							</ReactBootstrap.Modal.Dialog>
             </nav>
         );
     }
