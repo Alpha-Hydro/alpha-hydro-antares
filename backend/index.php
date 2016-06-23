3acab3e99800a7707e259e833acf257a2c7de50c
vladmeh <vladmeh@gmail.com>
2016-06-23 11:15:04 +0300

    update backend index autoload

--- backend/index.php
+++ backend/index.php
@@ -27,10 +27,16 @@ defined('UPLOAD_DIR')
 
 
 // Ensure library/ is on include_path
-set_include_path(implode(PATH_SEPARATOR, array(
+/*set_include_path(implode(PATH_SEPARATOR, array(
     realpath(APPLICATION_PATH . '/../../frameworks/Zend12/library'),
     get_include_path(),
+)));*/
+set_include_path(implode(PATH_SEPARATOR, array(
+    realpath(__DIR__ . '/../library'),
+    get_include_path(),
 )));
+
+require_once APPLICATION_ROOT . '/vendor/autoload.php';
 
 /** Zend_Application */
 require_once 'Zend/Application.php';
