003e46c865d461c660730831fb623a038dcf0bda
VladMeh <vladmeh@gmail.com>
2016-08-08 17:14:38 +0300

    add role item table admin

--- application/modules/admin/views/helpers/NavbarRightMenu.php
+++ application/modules/admin/views/helpers/NavbarRightMenu.php
@@ -12,6 +12,12 @@ class Zend_View_Helper_navbarRightMenu extends Zend_View_Helper_Abstract
         $config = new Zend_Config_Xml(APPLICATION_PATH.'/modules/admin/configs/navbar.xml', 'nav');
         $container = new Zend_Navigation();
         $container->setPages($config->toArray());
+        $container->addPage(
+            array(
+                'label'      => Zend_Auth::getInstance()->getIdentity()->email,
+                'title'      => 'Dashboard',
+                'uri'     => '/admin/',
+            ));
         $view = new Zend_View();
         echo $view->navigation($container)->menu()->setUlClass('nav navbar-nav navbar-right')->render();
     }
