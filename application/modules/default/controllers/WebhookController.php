72a00f8e41cf6cae566c638a2ea59628e36283bf
vladmeh <vladmeh@gmail.com>
2016-08-09 10:03:20 +0300

    webhook

--- application/modules/default/controllers/WebhookController.php
+++ application/modules/default/controllers/WebhookController.php
@@ -3,15 +3,9 @@
 class WebhookController extends Zend_Controller_Action
 {
     public function indexAction(){
-
-        if($this->getRequest()->isPost() && $this->getRequest()->getPost('payload')){
+        if($this->getRequest()->isPost() && $this->getRequest()->getPost('payload')) {
             $local_repo = APPLICATION_ROOT;
             shell_exec("cd {$local_repo} && git pull");
-        }
-        else{
-//            $this->getResponse()->setHttpResponseCode(404);
-//            $this->_helper->viewRenderer('404');
-            //throw new Zend_Exception("Page not found", 404);
         }
     }
 }
