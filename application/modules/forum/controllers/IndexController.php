8dee4351c0ded424759d0f56b23476951c7ba9c2
vladmeh <vladmeh@gmail.com>
2016-08-22 12:48:30 +0300

    test issue #44

--- application/modules/forum/controllers/IndexController.php
+++ application/modules/forum/controllers/IndexController.php
@@ -148,7 +148,6 @@ class Forum_IndexController extends Zend_Controller_Action
     public function sendAdminMail(Forum_Model_Forum $post)
     {
         $tr = new Zend_Mail_Transport_Smtp('mail.russ-call.ru');
-        Zend_Mail::setDefaultTransport($tr);
 
         $mailToAdmin = new Zend_Mail("UTF-8");
         //$mailToAdmin->setFrom($post->getEmail(), $post->getAuthor());
@@ -170,7 +169,7 @@ class Forum_IndexController extends Zend_Controller_Action
             "vladmeh@gmail.com",
             "mvl@alpha-hydro.com")
         );*/
-        $mailToAdmin->send();
+        $mailToAdmin->send($tr);
 
         return $this;
     }
