13919137ae6490bd319bdc40cddbfe0df1df374d
VladMeh <vladmeh@gmail.com>
2017-11-09 14:34:09 +0300

    update products.sql

--- data/products.sql
+++ data/products.sql
@@ -10,3 +10,10 @@ ALTER TABLE products ADD sorting TINYINT NOT NULL;
 UPDATE products SET sorting = `order`;
 
 ALTER TABLE products ADD deleted INT DEFAULT 0 NOT NULL;
+
+ALTER TABLE products ADD category_id INT NOT NULL AFTER id;
+CREATE INDEX category_id ON products (category_id);
+
+UPDATE products
+  INNER JOIN categories_xref ON products.id = categories_xref.product_id
+SET products.category_id = categories_xref.category_id;
